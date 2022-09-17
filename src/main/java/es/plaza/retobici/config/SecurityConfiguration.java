dd2a2ba07070c3c73f193b3031f441e961dd981c
jorge-plaza <jorgeplazalazo@disroot.org>
2022-09-17 13:20:13 +0200

    Added Spring Security and test for one endpoint

---
+++ src/main/java/es/plaza/retobici/config/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package es.plaza.retobici.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails user =  User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin =  User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> {
+                    auth.antMatchers("/").permitAll();
+                    auth.antMatchers("/api/v1/rewards/").hasRole("ADMIN");
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
