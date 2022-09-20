d3f64d1ae4d6c04592f2e5119862d25ca9ba4003
jorge-plaza <jorgeplazalazo@disroot.org>
2022-09-20 19:39:52 +0200

    Added Password encoder

--- src/main/java/es/plaza/retobici/config/SecurityConfiguration.java
+++ src/main/java/es/plaza/retobici/config/SecurityConfiguration.java
@@ -2,12 +2,15 @@ package es.plaza.retobici.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,15 +19,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder encoder){
         UserDetails user =  User.withDefaultPasswordEncoder()
                 .username("user")
-                .password("password")
+                .password(encoder.encode("password"))
                 .roles("USER")
                 .build();
         UserDetails admin =  User.withDefaultPasswordEncoder()
                 .username("admin")
-                .password("password")
+                .password(encoder.encode("password"))
                 .roles("ADMIN")
                 .build();
 
@@ -41,5 +44,10 @@ public class SecurityConfiguration {
                 })
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
